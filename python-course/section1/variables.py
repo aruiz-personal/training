94b6c7472fc1fe77783a0f869a3f05826e676706
Andres Ruiz <andres.ruiz@caylent.com>
2023-12-24 17:57:25 -0300

    Start github actions tests

---
+++ python-course/section1/variables.py
@@ -0,0 +1,38 @@
+#Numbers:
+
+float_division = 12 / 5
+
+print(float_division)
+
+integer_division = 12 // 5
+
+print(integer_division)
+
+remainder_of_a_division = 5 % 2
+
+print(remainder_of_a_division)
+
+#Strings:
+
+string_variable = "Hello world!"
+
+multi_line = """
+Hello
+World!
+"""
+
+print(multi_line)
+
+#It can be use as a comment as well:
+"""
+This is like a comment because we are doing nothing with the string variable that is created
+here. hehe
+"""
+
+#Concat:
+
+x = "Hello"
+
+age = 33
+
+print(x+" Andres, you are "+str(age))
