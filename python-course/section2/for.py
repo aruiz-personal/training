36c4c3801c8d40e390a08561d47fd3e0f6614fe1
Andres Sebatian Ruiz <asruiz.sis@gmail.com>
2024-01-06 20:02:57 -0300

    workflow for expressions and contexts

--- python-course/section2/for.py
+++ python-course/section2/for.py
@@ -22,3 +22,32 @@ friends3 = [("Rolf",25),("Andrew",33)]
 
 for name,age in friends3:
     print(f"Name: {name}, Age: {age}")
+
+# Iterating over dictionaries
+
+friends4 = {"Rolf": 23,"Andrew":33}
+
+for name in friends4:
+    print(name)
+
+for age in friends4.values():
+    print(age)
+
+for name,age in friends4.items():
+    print(f"{name},{age}")
+
+# Break and cotinue
+    
+cars = ["ok","ok","faulty","ok"]
+
+for status in cars:
+    if status == "faulty":
+        print("Stop")
+        break
+    print(status)
+
+for status in cars:
+    if status == "faulty":
+        print("Skip")
+        continue
+    print(status)
